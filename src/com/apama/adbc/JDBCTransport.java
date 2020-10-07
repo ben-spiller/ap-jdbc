1d91aac63778aa4aa1d4c79502a491d4f98de535
Richard Thrippleton <richard.thrippleton@softwareag.com>
2020-10-07 20:26:58 +0100

    Remove Command and Query, subsumed by Statement

--- src/com/apama/adbc/JDBCTransport.java
+++ src/com/apama/adbc/JDBCTransport.java
@@ -108,11 +108,7 @@ public class JDBCTransport extends AbstractSimpleTransport {
 		// Unique id generic to all events from EPL, used in acknowledgements
 		long messageId = payload.get("messageId", -1L);
 
-		if (eventType.equals("StartQuery")){
-			executeQuery(payload, m, messageId);
-		} else if (eventType.equals("Command")) {
-			executeCommand(messageId, payload);
-		} else if (eventType.equals("Store")) {
+		if (eventType.equals("Store")) {
 			executeStore(messageId, payload);
 		} else if (eventType.equals("Statement")) {
 			executeStatement(payload, m, messageId);
@@ -125,7 +121,6 @@ public class JDBCTransport extends AbstractSimpleTransport {
 		
 		try {
 			String sql_string = payload.getStringDisallowEmpty("sql"); 
-			logger.info("STATEMENT: " + sql_string);
 			PreparedStatement stmt = jdbcConn.prepareStatement(sql_string);
 
 			boolean resultsAvailable = stmt.execute();
@@ -199,17 +194,6 @@ public class JDBCTransport extends AbstractSimpleTransport {
 		}
 	}
 
-	private void executeCommand(long messageId, MapExtractor payload) throws SQLException {
-		String operation = payload.getStringDisallowEmpty("operationString");
-		Statement stmt = jdbcConn.createStatement();
-		stmt.executeUpdate(operation);
-		stmt.close();
-
-		Message ack = new Message(Collections.singletonMap("messageId", messageId),
-		                          Collections.singletonMap(Message.HOST_MESSAGE_TYPE, "com.apama.adbc.CommandAck"));
-		hostSide.sendBatchTowardsHost(Collections.singletonList(ack));
-	}
-
 	private void executeStore(long messageId, MapExtractor payload) throws SQLException {
 		String tableName = payload.getStringDisallowEmpty("tableName");
 		ResultSet tableSchema = jdbcConn.getMetaData().getColumns(null, null, tableName, null);
@@ -254,162 +238,6 @@ public class JDBCTransport extends AbstractSimpleTransport {
 		}
 		stmtActual.execute();
 		stmtActual.close();
-	}
-
-	public void executeQuery(MapExtractor payload, Message m, long messageId) throws Exception{ // TODO: this method is too long; refactor into smaller methods
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-		String errorPrefix = "Error executing query";
-		List<Message> msgList = new ArrayList<>();
-		long lastEventTime = 0;
-		Integer rowId = 0;
-		
-		Map<String, Object> queryDonePayload = new HashMap<>();
-		queryDonePayload.put("messageId", messageId);
-		//queryDonePayload.put("lastEventTime", lastEventTime); // 
-		Message queryDoneMsg = new Message(queryDonePayload);
-		queryDoneMsg.putMetadataValue(Message.HOST_MESSAGE_TYPE, "com.apama.adbc.QueryDone");
-
-		
-		try {
-			String queryString = payload.getStringDisallowEmpty("query"); 
-			logger.info("QUERY: " + queryString);
-			stmt = jdbcConn.prepareStatement(queryString);
-			logger.info("Executing query '" + queryString + "'");
-
-			// Execute the query
-			boolean resultsAvailable = true;
-			rs = stmt.executeQuery(); // TODO: cope with non-query statements that return multiple ResultSet's; also shoudl use try-with-using here
-
-			//AbstractRelationalDatabase.SchemaAttribute[] info = null;
-			ResultSetMetaData rsmd = null;
-			ParameterMetaData pmd = null;
-			int numColumns = 0;
-			int schemaId = 1;			
-			
-			while (resultsAvailable) {
-				if (rs != null) {
-					rsmd = rs.getMetaData();
-					numColumns = rsmd.getColumnCount();
-				}				
-			
-				sendSchemaEvent(stmt, rsmd, messageId);
-
-				// Fetch the data and send the events to the decoder
-				while (rs.next()) {
-					Map <String, Object> rowMap = new HashMap<>();
-
-					logger.info("After get RS.next" + queryString + "'");
-					// Add schema id to result event
-					//addSchemaId(schemaId);
-	
-					// Add column name:value pairs to the result event
-					int mode = ParameterMetaData.parameterModeUnknown;
-					String rsColumnName;
-					String columnName;
-					String columnValue;
-					int columnNameSuffix = 1;
-					for (int i=1; i<=numColumns; i++) { 
-						rsColumnName = rsmd.getColumnName(i);
-						//columnName = info[i-1].getName();
-						columnValue = rs.getString(i); // TODO: what about other types?
-						logger.info("ROW: FieldName: " + rsColumnName + " Value: " + columnValue);				
-	
-						if (rsColumnName == null || rsColumnName.length() == 0) {
-							logger.warn("Column #" +  i + " name is NULL, skipping");
-							break;
-						}
-	
-						if (columnValue != null) {
-							rowMap.put(rsColumnName,  columnValue);
-						}
-						// Do nothing for NULL values, they are not added to the payload
-					}
-	
-					//accumulate a batch of events and send back to the host when the batch is full.
-					if (rowMap.size() > 0){
-						Map<String, Object> resultPayload = new HashMap<>();
-						resultPayload.put("row", rowMap);
-						resultPayload.put("messageId", messageId);
-						rowId = rowId + 1;
-						//logger.info("For query id " + messageId + " Send row Id " + rowId);
-						resultPayload.put("rowId", rowId);
-						Message resultMsg = new Message(resultPayload);
-						resultMsg.putMetadataValue(Message.HOST_MESSAGE_TYPE, "com.apama.adbc.ResultEvent");
-						msgList.add(resultMsg);
-						if (msgList.size() >= batchSize){
-							lastEventTime = System.currentTimeMillis();	
-							// Send the result event(s) to the Host
-							hostSide.sendBatchTowardsHost(msgList);
-							msgList.clear();
-						}
-					}
-				}
-								
-				// Get the next result set
-				resultsAvailable = stmt.getMoreResults();
-				if (resultsAvailable) {
-					rs = stmt.getResultSet();
-					schemaId++;
-				}
-			}
-			//Send any remaining resultEvents to the host.
-			if (msgList.size() >0){		
-				lastEventTime = System.currentTimeMillis();	// TODO: why would this timestamp be useful?
-				// Send the result event(s) to the Host
-				hostSide.sendBatchTowardsHost(msgList);
-			}
-			else{
-				//no results
-			}
-
-			//send ack even if no results - QueryDone
-			queryDonePayload.put("errorMessage", "");
-			queryDonePayload.put("eventCount", msgList.size());
-			queryDonePayload.put("lastEventTime", lastEventTime);
-			hostSide.sendBatchTowardsHost(Collections.singletonList(queryDoneMsg));
-
-			
-		}
-		//TODO in future need to handle connection errors here (as well as other places) and retry to reopen the connection if its not open
-		/*catch (DataSourceException dex) {
-			//String message = errorPrefix + ", " + dex.getMessage();
-			//throw new DataSourceException(message, dex.isDisconnected());
-		}*/
-		catch (SQLException ex) {
-			String message = getSQLExceptionMessage(ex, errorPrefix);
-			
-			//Send QueryDone with errormsg
-			queryDonePayload.put("errorMessage", message);
-			queryDonePayload.put("eventCount", msgList.size());
-			hostSide.sendBatchTowardsHost(Collections.singletonList(queryDoneMsg));
-
-			throw new Exception(message);//, db.isDisconnected(con));
-		}
-		/*catch (Exception excp) { // is there really any other kind of exception we need to catch here?
-			String message = getExceptionMessage(excp, errorPrefix);
-			logger.debug(errorPrefix, excp);
-
-			//Send QueryDone with errormsg
-			Map<String, Object> queryDonePayload = new HashMap<>();
-			queryDonePayload.put("messageId", messageId);
-			queryDonePayload.put("errorMessage", message);
-			queryDonePayload.put("eventCount", msgList.size());
-			queryDonePayload.put("lastEventTime", lastEventTime);
-			Message queryDoneMsg = new Message(queryDonePayload);
-			queryDoneMsg.putMetadataValue(Message.HOST_MESSAGE_TYPE, "com.apama.adbc.QueryDone");
-
-			hostSide.sendBatchTowardsHost(Collections.singletonList(queryDoneMsg));
-
-			throw new Exception(message);//, db.isDisconnected(con));
-		}*/
-		finally {
-			// clean up
-			if (rs != null)	try	{rs.close();} catch(SQLException ex) {}
-			if (stmt != null) try {stmt.close();} catch(SQLException ex) {}
-			stmt = null;
-			rs = null;
-		}
 	}
 
 	public void sendSchemaEvent(Statement stmt, ResultSetMetaData rsmd, long messageId) throws SQLException
